4fe4573654a516253606272398ca8b5d81e44d58
Jeferson Takumi <tjefersonht@fastshop.com.br>
2022-04-25 21:37:55 -0300

    test card

--- lib/app/login_page.dart
+++ lib/app/login_page.dart
@@ -43,51 +43,68 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 100,
-                height: 100,
-                child: Image.asset(
-                  'assets/images/git.png',
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 16, right: 16, top: 16, bottom: 32),
+                  child: Column(children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      child: Image.asset(
+                        'assets/images/git.png',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      onChanged: (text) {
+                        email = text;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'E-mail',
+                        border: const OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      onChanged: (text) {
+                        senha = text;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        onPressed: () {
+                          if (email == 'jefersontakumi@gmail.com' &&
+                              senha == '123') {
+                            Navigator.of(context).pushReplacementNamed('/home');
+                          } else {
+                            print('Falha');
+                          }
+                        },
+                        child: Text(
+                          'Entrar',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    )
+                  ]),
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextField(
-                onChanged: (text) {
-                  email = text;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  border: const OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextField(
-                onChanged: (text) {
-                  senha = text;
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              RaisedButton(
-                onPressed: () {
-                  if (email == 'jefersontakumi@gmail.com' && senha == '123') {
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  } else {
-                    print('Falha');
-                  }
-                },
-                child: Text('Entrar'),
               )
             ],
           ),
