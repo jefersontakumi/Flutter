9083c03c17058d306bc5e6c7a59132303367b644
Jeferson Takumi <tjefersonht@fastshop.com.br>
2022-04-25 21:55:43 -0300

    add drawer

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -17,6 +17,33 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+                currentAccountPicture: Image.network(
+                    'https://tadarak.com/wp-content/uploads/2021/02/Male-Placeholder.png'),
+                accountName: Text('Jeferson Takumi'),
+                accountEmail: Text('jefersontakumi@gmail.com')),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Inicio'),
+              subtitle: const Text('Tela de inicio'),
+              onTap: () {
+                print('Home');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              subtitle: const Text('Finalizar sessão'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: const Text('Home Page'),
         actions: [CustomSwitch()],
@@ -27,39 +54,16 @@ class HomePageState extends State<HomePage> {
         child: ListView(
           scrollDirection: Axis.vertical,
           children: [
-            Text('Teste'),
-            CustomSwitch(),
-            Text('Texto'),
-            Container(
-              height: 50,
-            ),
-            Text('Texto'),
-            Container(
-              height: 50,
-            ),
-            Text('Texto'),
-            Container(
-              height: 50,
-            ),
-            Text('Texto'),
-            Container(
-              height: 50,
-            ),
-            Text('Texto'),
-            Container(
-              height: 50,
-            ),
-            Text('Texto'),
-            Container(
-              height: 50,
-            ),
-            Text('Texto'),
-            Container(
-              height: 50,
-            ),
-            Text('Texto'),
-            Container(
-              height: 50,
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(children: [
+                  Text('Teste'),
+                  CustomSwitch(),
+                ]),
+              ),
             )
           ],
         ),
