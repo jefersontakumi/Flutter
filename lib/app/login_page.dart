ca91a957af742549ec17a82c9f5e7035ee60032c
Jeferson Takumi <tjefersonht@fastshop.com.br>
2022-04-25 21:31:30 -0300

    stack view

--- lib/app/login_page.dart
+++ lib/app/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:startup_namer/app/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -15,63 +14,82 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset(
-                    'assets/images/git.png',
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'E-mail',
-                    border: const OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    senha = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    if (email == 'jefersontakumi@gmail.com' && senha == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('Falha');
-                    }
-                  },
-                  child: Text('Entrar'),
-                )
-              ],
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/background.png',
+              fit: BoxFit.cover,
             ),
+          ),
+          Container(
+            color: Colors.red.withOpacity(0.3),
+          ),
+          _body(),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                child: Image.asset(
+                  'assets/images/git.png',
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                  border: const OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                onChanged: (text) {
+                  senha = text;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  if (email == 'jefersontakumi@gmail.com' && senha == '123') {
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  } else {
+                    print('Falha');
+                  }
+                },
+                child: Text('Entrar'),
+              )
+            ],
           ),
         ),
       ),
