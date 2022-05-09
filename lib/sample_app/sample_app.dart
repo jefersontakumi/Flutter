fa416c1b02c248c9a28aa287acacf2ec4c1f98bf
Jeferson Takumi <tjefersonht@fastshop.com.br>
2022-05-09 20:56:37 -0300

    add parameter title

--- lib/sample_app/sample_app.dart
+++ lib/sample_app/sample_app.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:startup_namer/sample_app/sample_app_page.dart';
 
 class SampleApp extends StatelessWidget {
+  final String title;
+
   // This widget is the root of your application.
-  const SampleApp({Key? key}) : super(key: key);
+  const SampleApp({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Sample App',
-      home: SampleAppPage(),
+    return MaterialApp(
+      title: title,
+      home: const SampleAppPage(),
     );
   }
 }
